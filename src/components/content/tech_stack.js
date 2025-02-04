121b38c3275dd3c535b4ac44ef4818bafd9a200a
Jhane <allenjhane@gmail.com>
2025-02-04 15:52:44 -0800

    styles added to the tech stack when hovered

--- src/components/content/tech_stack.js
+++ src/components/content/tech_stack.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import "../../styles.css";
 import { 
     FaReact, 
     FaNodeJs, 
@@ -22,18 +23,18 @@ const TechStack = () => {
         <div className="mt-6 text-center">
             <h2 className="text-2xl font-bold mb-3">Tech Stack</h2>
             <div className="flex space-x-6 text-3xl">
-                <FaReact className="text-blue-400" title="React" />
-                <FaNodeJs className="text-green-500" title="Node.js" />
-                <FaPython className="text-yellow-500" title="Python" />
-                <FaJava className="text-red-500" title="Java" />
-                <SiCplusplus className="text-blue-600" title="C++" />
-                <SiMongodb className="text-green-700" title="MongoDB" />
-                <FaDatabase className="text-gray-600" title="Microsoft SQL Server" />
-                <SiGooglecloud className="text-blue-400" title="Google Cloud Platform" />
-                <SiFirebase className="text-yellow-500" title="Firestore" />
-                <FaHtml5 className="text-orange-600" title="HTML" />
-                <FaCss3Alt className="text-blue-600" title="CSS" />
-                <FaJs className="text-yellow-400" title="JavaScript" />
+                <FaReact className="text-blue-400 tech-icon" title="React" />
+                <FaNodeJs className="text-green-500 tech-icon" title="Node.js" />
+                <FaPython className="text-yellow-500 tech-icon" title="Python" />
+                <FaJava className="text-red-500 tech-icon" title="Java" />
+                <SiCplusplus className="text-blue-600 tech-icon" title="C++" />
+                <SiMongodb className="text-green-700 tech-icon" title="MongoDB" />
+                <FaDatabase className="text-gray-600 tech-icon" title="Microsoft SQL Server" />
+                <SiGooglecloud className="text-blue-400 tech-icon" title="Google Cloud Platform" />
+                <SiFirebase className="text-yellow-500 tech-icon" title="Firestore" />
+                <FaHtml5 className="text-orange-600 tech-icon" title="HTML" />
+                <FaCss3Alt className="text-blue-600 tech-icon" title="CSS" />
+                <FaJs className="text-yellow-400 tech-icon" title="JavaScript" />
             </div>
         </div>
     );
