b4d3cade812825194d52fc6aa111043bf9ec4301
Jhane <allenjhane@gmail.com>
2025-02-09 20:11:56 -0800

    testing

--- src/app.js
+++ src/app.js
@@ -2,6 +2,7 @@ import React from "react";
 import Portfolio from "./components/portfolio";
 
 const App = () => {
+  console.log("Backend URL:", process.env.REACT_APP_BACKEND_URL);
   return (
     <div>
       <Portfolio />
