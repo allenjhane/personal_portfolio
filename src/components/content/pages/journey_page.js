23228b47da3c542b673a813915347adfd0d2a0a3
Jhane <allenjhane@gmail.com>
2025-05-30 19:27:18 -0700

    updated journey page component for air force experience

--- src/components/content/pages/journey_page.js
+++ src/components/content/pages/journey_page.js
@@ -11,17 +11,45 @@ const lineColor = "#FAFAFA"; // Line color
 const JourneyPage = () => {
     return (
         <div className="flex flex-col items-center">
-            <h2 className="text-2xl font-bold mb-3">My Journey</h2>
+            <h2 className="text-2xl font-bold mb-3">My Career Journey</h2>
             <VerticalTimeline lineColor={lineColor}>
                 <VerticalTimelineElement className="vertical-timeline-element--work" 
-                                            date={<span style={{ padding: '8px' }}>January 2019</span>}
-                                            icon={<FaJetFighter />} 
-                                            iconStyle={{ background: lightThemePink, 
-                                                        color: 'black', 
-                                                        boxShadow: "none",
-                                                        border: `4px solid ${lineColor}` }}
-                                            contentStyle={{ boxShadow: 'none' }}>
-                    <h3 className="vertical-timeline-element-title text-black">Joined the Air Force</h3>
+                                            date={<span 
+                                                style={{ 
+                                                    marginTop: '-10px',  // Move date up to align with icon
+                                                    marginLeft: '50px',
+                                                    display: 'inline-block'   
+                                                }}
+                                                >January 2019
+                                                </span>}
+                                            icon={<img 
+                                                    src="bmt.jpg" 
+                                                    alt="Air Force" 
+                                                    style={{ 
+                                                        width: '100%', 
+                                                        height: '100%', 
+                                                        objectFit: 'cover',
+                                                        borderRadius: '50%'
+                                                    }}
+                                                />} 
+                                            iconStyle={{ 
+                                                background: lightThemePink, 
+                                                color: 'black', 
+                                                // boxShadow: "none",
+                                                border: `4px solid ${lineColor}`,
+                                                width: '100px',        // Add this - default is usually 40px
+                                                height: '100px',       // Add this - default is usually 40px
+                                                marginLeft: '-50px'   // half of width
+                                            }}
+                                            contentStyle={{ 
+                                                boxShadow: 'none',
+                                                marginTop: '10px',
+                                                marginLeft: '-20px',
+                                                display: 'inline-block' 
+                                                }}>
+                    <h3 className="vertical-timeline-element-title text-black">
+                        Joined the Air Force as a Radio IT Technician.
+                        </h3>
                 </VerticalTimelineElement>
                 <VerticalTimelineElement className="vertical-timeline-element--work" 
                                             date={<span style={{ padding: '8px' }}>July 2024</span>}
@@ -31,7 +59,22 @@ const JourneyPage = () => {
                                                         boxShadow: "none",
                                                         border: `4px solid ${lineColor}`  }}
                                             contentStyle={{ boxShadow: 'none' }}>
-                    <h3 className="vertical-timeline-element-title text-black">Internship at NASA</h3>
+                    <h3 className="vertical-timeline-element-title text-black">
+                        Accepted Internship at NASA as a Software Engineer Intern through the SkillBridge Program.
+                        </h3>
+                </VerticalTimelineElement>
+                <VerticalTimelineElement className="vertical-timeline-element--work" 
+                                            date={<span style={{ padding: '8px' }}>January 2025</span>}
+                                            icon={<LuSparkles />}
+                                            iconStyle={{ background: lightThemePink, 
+                                                        color: 'black',
+                                                        boxShadow: "none",
+                                                        border: `4px solid ${lineColor}`  }}
+                                            contentStyle={{ boxShadow: 'none' }}>
+                    <h3 className="vertical-timeline-element-title text-black">
+                        Transitioned out the military and continued my internship work at NASA 
+                        as the Lead Software Engineering Intern.
+                    </h3>
                 </VerticalTimelineElement>
             </VerticalTimeline>
         </div>
